ed969077a2e815a6e0a1dde9fd23846ba93df714
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2021-12-03 18:05:49 +0530

    skeuomorphic switch

---
+++ Skeuomorphic Button Effect/main.js
@@ -0,0 +1,24 @@
+
+let ch = document.getElementsByClassName('check')[0];
+let d = document.getElementsByClassName('buttons')[0];
+let o = document.getElementsByClassName('o-icon')[0];
+let x = document.getElementsByClassName('x-icon')[0];
+
+ch.addEventListener('change',()=>{
+    if(ch.checked)
+    {    
+        d.classList.add('switched');
+        o.style.fontSize = '22px';
+        o.style.color = 'green';
+        x.style.fontSize = '18px';
+        x.style.color = 'red';
+    }
+    else
+    {
+        d.classList.remove('switched');
+        x.style.fontSize = '22px';
+        x.style.color = 'green';
+        o.style.fontSize = '16px';
+        o.style.color = 'red';
+    }
+});
