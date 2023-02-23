d68920160f136ec2d71a8195f94026de7fef7536
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-02-23 19:43:00 +0530

    time updation and little animation

---
+++ Digital Clock/script.js
@@ -0,0 +1,33 @@
+const hr = document.querySelector('#hour');
+const mn = document.querySelector('#minute');
+const sc = document.querySelector('#second');
+
+setInterval(() => {
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * 6;
+    let ss = day.getSeconds() * 6;
+
+    let htime = day.getHours();
+    let mtime = day.getMinutes();
+    let stime = day.getSeconds();
+
+    if (htime < 10) {
+        htime = "0" + htime;
+    }
+    if (mtime < 10) {
+        mtime = "0" + mtime;
+    }
+    if (stime < 10) {
+        stime = "0" + stime;
+    }
+
+    hr.innerHTML = htime;
+    mn.innerHTML = mtime;
+    sc.innerHTML = stime;
+
+
+    hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
+    mn.style.transform = `rotateZ(${mm}deg)`;
+    sc.style.transform = `rotateZ(${ss}deg)`;
+})
