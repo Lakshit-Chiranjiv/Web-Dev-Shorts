25ba48f40ecdcd1414002f4b8800c19a2c07d46a
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2023-02-26 01:32:29 +0530

    got all dom elements

---
+++ Gender Decider/script.js
@@ -0,0 +1,15 @@
+const nameInput = document.querySelector('#nameInput');
+const btn = document.querySelector('#btn');
+
+const err = document.querySelector('#error');
+
+const outputDiv = document.querySelector('.outputDiv');
+const descDiv = document.querySelector('.desc');
+const loadDiv = document.querySelector('.load');
+const contentDiv = document.querySelector('.content');
+
+const output = document.querySelector('#output');
+const name = document.querySelector('#name');
+const maleImg = document.querySelector('#male');
+const femaleImg = document.querySelector('#female');
+const prob = document.querySelector('#prob');
